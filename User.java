35e299c5f4d6ecd0fc4ba0be910aadf245445cb7
JX531 <91365967+JX531@users.noreply.github.com>
2023-11-25 10:11:16 +0800

    Update User.java overloaded constructor

--- User.java
+++ User.java
@@ -1,23 +1,40 @@
+import java.util.ArrayList;
+
 public abstract class User {
+    private String Name;
     private String userID;
     private String password;
     private String faculty;
 
-    public User(String userID, String faculty) {
+    public User(String name,String userID, String faculty) {
+        this.Name = name;
         this.userID = userID;
         this.faculty = faculty;
-        password = "password";
+        this.password = "password";
     }
+    //Overloaded
+    public User(String name,String userID, String faculty, String password) {
+        this.Name = name;
+        this.userID = userID;
+        this.faculty = faculty;
+        this.password = password;
+    }
+
 
     public String getID() {
         return this.userID;
     }
-
+    public String getName(){
+        return this.Name;
+    }
+    public String getPassword(){
+        return this.password;
+    }
     public boolean checkPassword(String password) {
         return (this.password.equals(password));
     }
 
-    public String getFaulty() {
+    public String getFaculty() {
         return this.faculty;
     }
 
@@ -25,5 +42,7 @@ public abstract class User {
         this.password = password;
     }
 
-    public void menu(ArrayList<Camp> CampMasterList) {}
+    public void menu(ArrayList<Camp> CampMasterList) {
+        
+    }
 }
